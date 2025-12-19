8211942cd9143b6d7eb7d5c136e40f47c2fa3ae8
PalakGoyal2307 <palakgoyal237@gmail.com>
2025-12-20 01:57:40 +0530

    Describe your changes here

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,12 @@ import subscriberRoutes from "./routes/subscriberRoutes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
@@ -32,3 +37,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Server running on port ${PORT}`)
 );
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
